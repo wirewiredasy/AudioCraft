03bc30c3072d42a07befdcb482a8b8e70ff2c762
wirewiredasy <webwireasy.dev@gmail.com>
2025-08-07 18:06:19 +0000

    Workflow an

--- frontend/.next/server/middleware-build-manifest.js
+++ frontend/.next/server/middleware-build-manifest.js
@@ -23,6 +23,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
+    ],
+    "/support": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/support.js"
     ]
   },
   "ampFirstPages": []
