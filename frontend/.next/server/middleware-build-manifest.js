357d54e0b660222433379139540cc45a3fee363a
wirewiredasy <webwireasy.dev@gmail.com>
2025-08-07 13:12:43 +0000

    Fix

--- frontend/.next/server/middleware-build-manifest.js
+++ frontend/.next/server/middleware-build-manifest.js
@@ -23,6 +23,16 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
+    ],
+    "/tools": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/tools.js"
+    ],
+    "/tools/fade-effect": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/tools/fade-effect.js"
     ]
   },
   "ampFirstPages": []
